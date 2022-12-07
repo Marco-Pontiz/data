7c73b2eeb04f48bb34340c3c240335af42cb4fb6
Marco Pontiz <marcopontiz@gmail.com>
2022-12-06 23:56:43 -0300

    data

---
+++ src/config/configDB.js
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const options = {
+  mysql: {
+    client: "mysql2",
+    connection: {
+      host: "localhost",
+      user: 'root',
+      password: 'Azulyrojo300597',
+      database: 'Local instance MysSQL80',
+    },
+    pool: { min: 0, max: 10 },
+  },
+  sqlite: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/ecommerce.sqlite",
+    },
+    useNullAsDefault: true,
+  },
+};
